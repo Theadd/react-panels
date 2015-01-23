725c16262edd54d6673648eb3513b05381562d09
Theadd <pantallazo@gmail.com>
2015-01-23 22:51:22 +0100

    misc

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# rpanel
+# react-panels
+===========
+### [Website](http://theadd.github.io/react-panels/)
+
+A panel widget with extended features made with React and LESS.
+
+<img src="http://theadd.github.io/react-panels/images/example.png" title="Example"/>
--- assets/js/demo.js
+++ assets/js/demo.js
@@ -38,29 +38,41 @@ var panel1 = (
       icon="fa fa-area-chart"
       title="Area Chart"
       toolbarState="visible"
-      toolbar={(<span>Toolbar content here</span>)}>
-      <div>Panel content here</div>
+      toolbar={(<span><input type="text" placeholder="Example text input in toolbar" className="input-example" /></span>)}>
+      <div>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        <br /><br />
+        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+      </div>
     </PanelContent>
     <PanelContent
       icon="fa fa-pie-chart"
       title="Pie Chart"
       toolbarState="hidden"
-      toolbar={(<span>Toolbar content for pie chart here</span>)}>
-      <div>Panel content of pie chart here</div>
+      toolbar={(<span><input type="text" placeholder="Example text input in toolbar" className="input-example" /></span>)}>
+      <div>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+      </div>
     </PanelContent>
     <PanelContent
       icon="fa fa-line-chart"
       title="Line Chart"
       toolbarState="locked"
-      toolbar={(<span>Toolbar content for line chart here</span>)}>
-      <div>Panel content of line chart here</div>
+      toolbar={(<span><input type="text" placeholder="Example text input in toolbar" className="input-example" /></span>)}>
+      <div>
+        At solmen va esser necessi far uniform grammatica, pronunciation e plu sommun paroles. Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam ti del coalescent lingues.
+      </div>
     </PanelContent>
     <PanelContent
       icon="fa fa-bar-chart"
       title="Bar Chart"
       toolbarState="none"
-      toolbar={(<span>Toolbar content for bar chart here</span>)}>
-      <div>Panel content of bar chart here</div>
+      toolbar={(<span><input type="text" placeholder="Example text input in toolbar" className="input-example" /></span>)}>
+      <div>
+        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+        <br /><br />
+        Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+      </div>
     </PanelContent>
   </Panel>
 );
