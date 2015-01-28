72cbfb49997e63cb1c70a7b619e31f099d790927
Theadd <pantallazo@gmail.com>
2015-01-28 02:04:35 +0100

    more examples and update to react-panels v0.3.2

---
+++ bower_components/react-panels/Gruntfile.js
@@ -0,0 +1,63 @@
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    watch: {
+      styles: {
+        files: ['src/less/**'],
+        tasks: ['less'],
+        options: {
+          spawn: true,
+          interrupt: true
+        }
+      },
+      sources: {
+        files: ['src/jsx/**'],
+        tasks: ['react'],
+        options: {
+          spawn: true,
+          interrupt: true
+        }
+      }
+    },
+    less: {
+      development: {
+        options: {
+          paths: ["src/less"]
+        },
+        files: {
+          "dist/react-panels.css": "src/less/rpanel.less"
+        }
+      },
+      production: {
+        options: {
+          paths: ["src/less"],
+          compress: true
+        },
+        files: {
+          "dist/react-panels.min.css": "src/less/rpanel.less"
+        }
+      }
+    },
+    react: {
+      combined_file_output: {
+        files: {
+          'dist/react-panels.js': [
+            'src/jsx/rpanel.jsx'
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-react');
+
+  grunt.registerTask('default', ['less', 'react']);
+  grunt.registerTask('live', ['watch', 'less', 'react']);
+
+};
---
+++ bower_components/react-panels/README.md
@@ -0,0 +1,9 @@
+react-panels
+===========
+### [Website](http://theadd.github.io/react-panels/)
+
+A panel widget with extended features made with React and LESS.
+
+Tested in Google Chrome, Mozilla Firefox, Opera and Internet Explorer 10 & 11.
+
+<img src="http://theadd.github.io/react-panels/images/example.png" title="Example"/>
